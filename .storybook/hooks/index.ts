41130cfca109f0c1d5d3a04f51bbb80902167b18
Jonathan Siler <jonjonsiler@hotmail.com>
2026-01-27 11:19:24 -0600

    Add TS shim for skill resources

--- .storybook/hooks/index.ts
+++ .storybook/hooks/index.ts
@@ -4,5 +4,11 @@ export const usePlannerCompare = () => {};
 export const usePreTeachData = () => {};
 export const useReTeachData = () => {};
 export const useRosterInfo = () => {};
+export const useSkillResources = () => {
+  return {
+    getResources: (skillIds: string[], limit: number) => {},
+    resourceMap: {} as Record<string, any[]>
+  }
+};
 export * from './useSelectedClassroom';
 export const useWeeklyPlan = () => {};
